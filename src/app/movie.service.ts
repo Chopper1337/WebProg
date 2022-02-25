a1a0fb7c66697276c3543ca6ce47b01946bfc705
joseph <joseph@X6>
2022-02-25 04:14:35 +0000

    Week 5 worksheet completed. Issues still present, to be fixed.

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Movie } from './movie.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-
+  movie:Movie;
   constructor() { }
-  
-  addMovie(movietitle:string, moviedirector:string, movieyear:string){
-    this.moviesList.push({title:movietitle, director:moviedirector, year:movieyear});
-  }
-
   moviesList = [
     {title: 'Scarface', year: '1980', director: 'Jim Mark'},
     {title: 'Dawn of The Dead', year: '1990', director: 'Mike Samson'},
@@ -21,5 +17,9 @@ export class MovieService {
   
   getMovies() {
     return this.moviesList;
+  }
+  
+  addMovie(movietitle:string, moviedirector:string, movieyear:string){
+    this.moviesList.push(new Movie(movietitle, movieyear, moviedirector));
   }
 }
